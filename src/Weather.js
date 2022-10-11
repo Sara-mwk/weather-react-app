74d468e06be11f2b68c19f57871df2a761f54d46
Sara-mwk <106980251+Sara-mwk@users.noreply.github.com>
2022-10-11 14:23:02 -0500

    created weather-react

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+
+function handleResponse (response) {
+alert(`The weather in New York is ${response.data.main.temp}°C`);
+}   
+
+let apiKey = "8cfa4dd3ccafa97ae01716474ab8d486";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=$${apiKey}&units=mertic`;
+
+axios.get(apiUrl).then(handleResponse);
+
+    return (
+        <h2> Hello from Weather</h2>
+    );
+}
