718b08773cc1c6e1e931001d2089998e1a136c3c
Sara-mwk <106980251+Sara-mwk@users.noreply.github.com>
2022-10-12 13:56:42 -0500

    Updated Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,15 @@
 import React from "react";
-import axios from "axios";
+import axios from 'axios';
 
 export default function Weather(props) {
 
-function handleResponse (response) {
+function handleResponse(response) {
 alert(`The weather in New York is ${response.data.main.temp}°C`);
 }   
 
 let apiKey = "8cfa4dd3ccafa97ae01716474ab8d486";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=$${apiKey}&units=mertic`;
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(handleResponse);
-
     return (
         <h2> Hello from Weather</h2>
     );
